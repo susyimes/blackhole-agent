d0c3c229ebf5d7300f9c6f04ef4f06120ff3696a
honor00 <susyimes2@gmail.com>
2026-08-27 00:47:09 +0800

    Blackhole unbound milestone 1: feat: forage forty-six-level nested-namespace Python class instance methods

--- src/blackhole_agent/capability_foraging.py
+++ src/blackhole_agent/capability_foraging.py
@@ -164,7 +164,7 @@ from blackhole_agent.capability_compounder import (
 
 SCHEMA_VERSION = 1
 
-# Depth 0 is a top-level Class.method; depth 45 is a quinquadragintuple nested Class().method.
+# Depth 0 is a top-level Class.method; depth 46 is a sexquadragintuple nested Class().method.
 PYTHON_NESTED_CLASS_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_class",
     "python_nested_namespace_class",
@@ -212,6 +212,7 @@ PYTHON_NESTED_CLASS_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_trequadragintuple_nested_namespace_class",
     "python_quattuorquadragintuple_nested_namespace_class",
     "python_quinquadragintuple_nested_namespace_class",
+    "python_sexquadragintuple_nested_namespace_class",
 )
 PYTHON_NESTED_FUNCTION_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_nested_namespace_function",
@@ -259,6 +260,7 @@ PYTHON_NESTED_FUNCTION_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_trequadragintuple_nested_namespace_function",
     "python_quattuorquadragintuple_nested_namespace_function",
     "python_quinquadragintuple_nested_namespace_function",
+    "python_sexquadragintuple_nested_namespace_function",
 )
 
 
@@ -900,6 +902,7 @@ _CLASS_KIND_PREFIXES = (
     "python_trequadragintuple_nested_namespace_class",
     "python_quattuorquadragintuple_nested_namespace_class",
     "python_quinquadragintuple_nested_namespace_class",
+    "python_sexquadragintuple_nested_namespace_class",
 )
 
 
@@ -1136,6 +1139,7 @@ _FUNC_KEYS = (
     "python_trequadragintuple_nested_namespace_function",
     "python_quattuorquadragintuple_nested_namespace_function",
     "python_quinquadragintuple_nested_namespace_function",
+    "python_sexquadragintuple_nested_namespace_function",
 )
 level = submodules
 max_depth = len(_CLASS_KIND_PREFIXES) - 1
