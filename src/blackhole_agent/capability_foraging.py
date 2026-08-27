4639b9ea5576aec75f0babef8fda42d29ab0048d
honor00 <susyimes2@gmail.com>
2026-08-27 08:54:22 +0800

    Blackhole unbound milestone 1: feat: forage sixty-four-level nested-namespace Python class instance methods

--- src/blackhole_agent/capability_foraging.py
+++ src/blackhole_agent/capability_foraging.py
@@ -164,7 +164,7 @@ from blackhole_agent.capability_compounder import (
 
 SCHEMA_VERSION = 1
 
-# Depth 0 is a top-level Class.method; depth 63 is a tresexagintuple nested Class().method.
+# Depth 0 is a top-level Class.method; depth 64 is a quattuorsexagintuple nested Class().method.
 PYTHON_NESTED_CLASS_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_class",
     "python_nested_namespace_class",
@@ -230,6 +230,7 @@ PYTHON_NESTED_CLASS_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_unsexagintuple_nested_namespace_class",
     "python_duosexagintuple_nested_namespace_class",
     "python_tresexagintuple_nested_namespace_class",
+    "python_quattuorsexagintuple_nested_namespace_class",
 )
 PYTHON_NESTED_FUNCTION_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_nested_namespace_function",
@@ -295,6 +296,7 @@ PYTHON_NESTED_FUNCTION_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_unsexagintuple_nested_namespace_function",
     "python_duosexagintuple_nested_namespace_function",
     "python_tresexagintuple_nested_namespace_function",
+    "python_quattuorsexagintuple_nested_namespace_function",
 )
 
 
@@ -954,6 +956,7 @@ _CLASS_KIND_PREFIXES = (
     "python_unsexagintuple_nested_namespace_class",
     "python_duosexagintuple_nested_namespace_class",
     "python_tresexagintuple_nested_namespace_class",
+    "python_quattuorsexagintuple_nested_namespace_class",
 )
 
 
@@ -1208,6 +1211,7 @@ _FUNC_KEYS = (
     "python_unsexagintuple_nested_namespace_function",
     "python_duosexagintuple_nested_namespace_function",
     "python_tresexagintuple_nested_namespace_function",
+    "python_quattuorsexagintuple_nested_namespace_function",
 )
 level = submodules
 max_depth = len(_CLASS_KIND_PREFIXES) - 1
