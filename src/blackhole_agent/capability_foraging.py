eea80381def006e36140c6e7d7439296a01c450d
honor00 <susyimes2@gmail.com>
2026-08-28 15:05:30 +0800

    Blackhole unbound milestone 1: feat: forage one-hundred-seventeen-level nested-namespace Python class instance methods

--- src/blackhole_agent/capability_foraging.py
+++ src/blackhole_agent/capability_foraging.py
@@ -164,7 +164,7 @@ from blackhole_agent.capability_compounder import (
 
 SCHEMA_VERSION = 1
 
-# Depth 0 is a top-level Class.method; depth 116 is a sexdecicentuple nested Class().method.
+# Depth 0 is a top-level Class.method; depth 117 is a septendecicentuple nested Class().method.
 PYTHON_NESTED_CLASS_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_class",
     "python_nested_namespace_class",
@@ -283,6 +283,7 @@ PYTHON_NESTED_CLASS_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_quattuordecicentuple_nested_namespace_class",
     "python_quindecicentuple_nested_namespace_class",
     "python_sexdecicentuple_nested_namespace_class",
+    "python_septendecicentuple_nested_namespace_class",
 )
 PYTHON_NESTED_FUNCTION_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_nested_namespace_function",
@@ -401,6 +402,7 @@ PYTHON_NESTED_FUNCTION_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_quattuordecicentuple_nested_namespace_function",
     "python_quindecicentuple_nested_namespace_function",
     "python_sexdecicentuple_nested_namespace_function",
+    "python_septendecicentuple_nested_namespace_function",
 )
 
 
@@ -1113,6 +1115,7 @@ _CLASS_KIND_PREFIXES = (
     "python_quattuordecicentuple_nested_namespace_class",
     "python_quindecicentuple_nested_namespace_class",
     "python_sexdecicentuple_nested_namespace_class",
+    "python_septendecicentuple_nested_namespace_class",
 )
 
 
@@ -1420,6 +1423,7 @@ _FUNC_KEYS = (
     "python_quattuordecicentuple_nested_namespace_function",
     "python_quindecicentuple_nested_namespace_function",
     "python_sexdecicentuple_nested_namespace_function",
+    "python_septendecicentuple_nested_namespace_function",
 )
 level = submodules
 max_depth = len(_CLASS_KIND_PREFIXES) - 1
