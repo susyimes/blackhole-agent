63643ea329f3792c37f37307d51af9761335ece7
honor00 <susyimes2@gmail.com>
2026-08-28 14:35:17 +0800

    Blackhole unbound milestone 1: feat: forage one-hundred-sixteen-level nested-namespace Python class instance methods

--- src/blackhole_agent/capability_foraging.py
+++ src/blackhole_agent/capability_foraging.py
@@ -164,7 +164,7 @@ from blackhole_agent.capability_compounder import (
 
 SCHEMA_VERSION = 1
 
-# Depth 0 is a top-level Class.method; depth 115 is a quindecicentuple nested Class().method.
+# Depth 0 is a top-level Class.method; depth 116 is a sexdecicentuple nested Class().method.
 PYTHON_NESTED_CLASS_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_class",
     "python_nested_namespace_class",
@@ -282,6 +282,7 @@ PYTHON_NESTED_CLASS_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_tredecicentuple_nested_namespace_class",
     "python_quattuordecicentuple_nested_namespace_class",
     "python_quindecicentuple_nested_namespace_class",
+    "python_sexdecicentuple_nested_namespace_class",
 )
 PYTHON_NESTED_FUNCTION_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_nested_namespace_function",
@@ -399,6 +400,7 @@ PYTHON_NESTED_FUNCTION_DEPTH_PREFIXES: tuple[str, ...] = (
     "python_tredecicentuple_nested_namespace_function",
     "python_quattuordecicentuple_nested_namespace_function",
     "python_quindecicentuple_nested_namespace_function",
+    "python_sexdecicentuple_nested_namespace_function",
 )
 
 
@@ -1110,6 +1112,7 @@ _CLASS_KIND_PREFIXES = (
     "python_tredecicentuple_nested_namespace_class",
     "python_quattuordecicentuple_nested_namespace_class",
     "python_quindecicentuple_nested_namespace_class",
+    "python_sexdecicentuple_nested_namespace_class",
 )
 
 
@@ -1416,6 +1419,7 @@ _FUNC_KEYS = (
     "python_tredecicentuple_nested_namespace_function",
     "python_quattuordecicentuple_nested_namespace_function",
     "python_quindecicentuple_nested_namespace_function",
+    "python_sexdecicentuple_nested_namespace_function",
 )
 level = submodules
 max_depth = len(_CLASS_KIND_PREFIXES) - 1
